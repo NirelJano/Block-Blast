a7b5198c4d02a6b739eda88f28b58d6026d9bb95
NirelJano <nireljano@gmail.com>
2025-02-03 14:05:54 +0200

    יצירת שרת, קבצי html css js server, client

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// 📌 הגדרת תיקיית `public` כדי לטעון קבצים סטטיים
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
